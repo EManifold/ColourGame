a0f001c8785c6380d0ca69fa81f86d71017a2151
Ellie <elliemmanifold@gmail.com>
2019-11-01 13:51:08 +0000

    Create README and clean up code in js file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ColourGame
+
+This game was made using the tutorial provided by Colt Steele's Web Developer Bootcamp on Udemy.
+
+The idea of the game is to pick the colour that matches the RGB provided in the header. In the hard mode of the game, 6 colours are provided and you have to pick the one that matches the colour given. If you choose the incorrect colour tile, the tile disappears and message saying 'Try again' appears. This continues until you pick the correct tile, in which case all tiles reappear with in the colour of the correct RGB, along with a matching header bar colour. The easy mode of the game is the act same premise, but with only 3 tiles to choose from.
+
+Technologies used:
+- HTML
+- CSS
+- JavaScript
+
+## Easy mode
+<img width="1264" alt="Screenshot 2019-11-01 at 12 32 48" src="https://user-images.githubusercontent.com/53044792/68029035-442fc080-fcae-11e9-986a-e166b20eedce.png">
+
+## Hard mode
+<img width="1267" alt="Screenshot 2019-11-01 at 12 32 17" src="https://user-images.githubusercontent.com/53044792/68029095-66294300-fcae-11e9-9db4-4bc8de69e83f.png">
+
+## Picking a wrong colour tile in hard mode
+<img width="1264" alt="Screenshot 2019-11-01 at 12 33 05" src="https://user-images.githubusercontent.com/53044792/68029116-72150500-fcae-11e9-9b02-a28f3edfed7e.png">
+
+## Picking the correct colour tile in hard mode
+<img width="1264" alt="Screenshot 2019-11-01 at 12 33 16" src="https://user-images.githubusercontent.com/53044792/68029136-80632100-fcae-11e9-98fb-ee64260d86f4.png">
--- colorGame.js
+++ colorGame.js
@@ -1,12 +1,12 @@
-   var numSquares = 6;
-   var colors = []
+  var numSquares = 6;
+  var colors = []
 	var pickedColor;
 	var squares = document.querySelectorAll('.square')
 	var colorDisplay = document.getElementById("colorDisplay")
 	var messageDisplay = document.querySelector("#message")
 	var h1 = document.querySelector("h1")
 	var resetButton = document.querySelector('#reset')
-   var modeButtons = document.querySelectorAll(".mode")
+  var modeButtons = document.querySelectorAll(".mode")
 
    init()
 
@@ -23,7 +23,7 @@
    		modeButtons[1].classList.remove('selected')
    		this.classList.add('selected')
    		this.textContent === 'Easy'? numSquares = 3 : numSquares = 6;
-   		
+
    		reset()
    	})
      }
@@ -46,8 +46,6 @@
 	  }
    }
 
-   
-
    function reset() {
    	colors = generateRandomColors(numSquares);
 		pickedColor = pickColor();
@@ -65,43 +63,9 @@
 		h1.style.backgroundColor = 'steelblue';
    }
 
-
-	// easyBtn.addEventListener("click", function() {
-	// 	hardBtn.classList.remove('selected')
-	// 	easyBtn.classList.add("selected")
-	// 	numSquares = 3
-	// 	colors = generateRandomColors(numSquares)
-	// 	pickedColor = pickColor()
-	// 	colorDisplay.textContent = pickedColor;
-	// 	for (var i = 0; i < squares.length; i++) {
-	// 		if(colors[i]){
-	// 			squares[i].style.backgroundColor = colors[i]
-	// 		} else {
-	// 			squares[i].style.display = 'none'
-	// 		}
-	// 	}
-	// })
-
-	// hardBtn.addEventListener("click", function() {
-	// 	hardBtn.classList.add('selected')
-	// 	easyBtn.classList.remove('selected')
-	// 	numSquares = 6
-	// 	colors = generateRandomColors(numSquares)
-	// 	pickedColor = pickColor()
-	// 	colorDisplay.textContent = pickedColor;
-	// 	for (var i = 0; i < squares.length; i++) {
-	// 			squares[i].style.backgroundColor = colors[i]
-	// 			squares[i].style.display = 'block'
-	// 	}
-	// })
-
 	resetButton.addEventListener("click", function() {
 		reset()
 	})
-
-
-
-
 
 	function changeColors(color) {
 		for (let i = 0; i < squares.length; i++) {
@@ -116,12 +80,9 @@
 
 	function generateRandomColors(num) {
 		var arr = []
-
 		for (var i = 0; i < num; i++) {
 			arr.push(randomColor())
-
 		}
-
 		return arr;
 	}
 
@@ -131,35 +92,3 @@
 		var b = Math.floor(Math.random() * 256)
 		return "rgb(" + r + ", " + g + ", " + b + ")"
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
